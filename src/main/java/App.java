ea30d9c573c89e5b3f519373e703828ac37279f8
Alexander Smith <mr.alexander.smith@hotmail.com>
2024-03-17 23:39:49 +0300

    Task is done.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        var helloworld =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        var cat1 = applicationContext.getBean("cat", Cat.class);
+        var cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == helloworld);
+        System.out.println(cat1 == cat2);
     }
 }
